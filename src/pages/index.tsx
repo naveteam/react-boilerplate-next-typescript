015df21f7e955f90ea8fde4898c51f1e913ebd25
Igor Oliveira <devigor.tere@gmail.com>
2022-04-25 11:04:37 -0300

    chore: add prettier and eslint in the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1>
-      A well-structured production ready Next.js boilerplate with Typescript
-    </h1>
-  );
+  return <h1>A well-structured production ready Next.js boilerplate with Typescript</h1>
 }
