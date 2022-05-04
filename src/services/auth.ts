91764de4c3c46712eb69c7e6319943aa6abc5626
Igor Oliveira <devigor.tere@gmail.com>
2022-05-04 12:38:34 -0300

    feat: migrate services and providers

---
+++ src/services/auth.ts
@@ -0,0 +1,16 @@
+import client from 'providers/fetchClient'
+
+export type GetUserProps = {
+  id: string
+  name: string
+  email: string
+}
+
+export type SignInProps = {
+  email: string
+  password: string
+}
+
+export const getUser = () => client.get<GetUserProps>('/v1/me')
+
+export const login = data => client.post<SignInProps>('/v1/users/login', data)
