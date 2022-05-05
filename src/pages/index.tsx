0824f9a101f11073709a7a595db6dd695c6fdf87
Igor Oliveira <devigor.tere@gmail.com>
2022-05-05 09:49:08 -0300

    feat: create a better home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,1 @@
-import { Text } from 'components'
-import { useTheme } from 'styled-components'
-
-export default function Home() {
-  const { colors } = useTheme()
-  return (
-    <Text variant='big' color={colors.black}>
-      A well-structured production ready Next.js boilerplate with Typescript
-    </Text>
-  )
-}
+export { default } from 'routes/Home'
